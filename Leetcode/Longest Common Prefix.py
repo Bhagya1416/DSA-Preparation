3d24a6153b45236cd1e3b7a52abb32a62ad8925d
Bhagya Lakshmi <narapareddybhagyalakshmi@gmail.com>
2026-08-03 21:55:43 +0530

    Add solution for Longest Common Prefix problem

---
+++ Leetcode/Longest Common Prefix.py
@@ -0,0 +1,12 @@
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        if not strs:
+            return ""
+        first_str=strs[0]
+        for i in range(len(first_str)):
+            char=first_str[i]
+            for other_str in strs[1:]:
+                if i>=len(other_str) or other_str[i]!=char:
+                    return first_str[:i]
+        return first_str
+        
