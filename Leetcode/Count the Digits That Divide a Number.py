5ddf5a189ddfce811f1f4ef465d4b5cb8b9c7eac
Bhagya Lakshmi <narapareddybhagyalakshmi@gmail.com>
2026-09-14 21:37:26 +0530

    Add solution to count digits that divide a number

---
+++ Leetcode/Count the Digits That Divide a Number.py
@@ -0,0 +1,9 @@
+class Solution:
+    def countDigits(self, num: int) -> int:
+        count=0
+        for digit_char in str(num):
+            digit=int(digit_char)
+            if num%digit==0:
+                count+=1
+        return count
+        
